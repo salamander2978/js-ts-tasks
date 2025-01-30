d5b66f9b344a4aacf581176c99784c55fea1a711
salamander <timofeyika@gmail.com>
2025-01-30 15:20:52 +0200

    hw

--- src/user.js
+++ src/user.js
@@ -1,57 +1,60 @@
 class User {
-  // Your code for class User here
+  #firstName;
+  #secondName;
+  #age;
+
+  constructor(firstName, secondName, age) {
+      if (typeof firstName !== 'string') throw new Error('firstName must be a string');
+      if (typeof secondName !== 'string') throw new Error('secondName must be a string');
+      if (typeof age !== 'number') throw new Error('age must be a number');
+      
+      this.#firstName = firstName;
+      this.#secondName = secondName;
+      this.#age = age;
+  }
+
+  get age() {
+      return this.#age;
+  }
+
+  set age(value) {
+      if (typeof value !== 'number') throw new Error('age must be a number');
+      this.#age = value;
+  }
+
+  set firstName(value) {
+      if (typeof value !== 'string') throw new Error('firstName must be a string');
+      this.#firstName = value;
+  }
+
+  set secondName(value) {
+      if (typeof value !== 'string') throw new Error('secondName must be a string');
+      this.#secondName = value;
+  }
+
+  get name() {
+      return `${this.#firstName} ${this.#secondName}`;
+  }
+
+  introduce() {
+      return `My name is ${this.#firstName} ${this.#secondName}, I'm ${this.#age}`;
+  }
+
+  celebrateBirthday() {
+      this.#age += 1;
+  }
 }
 
-/**
- * Create a class named User
- * @type {User}
- */
-module.exports.User = User;
+const createUser = (firstName, secondName, age) => new User(firstName, secondName, age);
 
-/**
- * Create new User object and return it
- * @param {string} firstName
- * @param {string} secondName
- * @param {number} age
- * @returns {User}
- */
-module.exports.createUser = function (firstName, secondName, age) {
-  throw new Error('Not implemented'); // remove this line and put your code here
-};
+const createUsers = (data) => data.map(user => new User(user.firstName, user.secondName, user.age));
 
-/**
- * Create Array of Users by provided Array with user data (firstName, secondName, age)
- * @param {Array<{ firstName, secondName, age }>} data
- * @returns {Array<User>}
- */
-module.exports.createUsers = function (data) {
-  throw new Error('Not implemented'); // remove this line and put your code here
-};
+const findUsersByAge = (users, age) => users.filter(user => user.age === age);
 
-/**
- * Find Users in Array of Users who's age equals the provided age
- * @param {Array<Users>} users
- * @param {number} age
- * @returns {Array<Users>}
- */
-module.exports.findUsersByAge = function (users, age) {
-  throw new Error('Not implemented'); // remove this line and put your code here
-};
+const createUsersSortFn = (TestUtils) => (users) => users.sort(TestUtils.comparator);
 
-/**
- * Return a function that sort provided Array of Users using a comparator function from TestUtils
- * @param TestUtils
- * @returns {function(*): *[]}
- */
-module.exports.createUsersSortFn = function (TestUtils) {
-  throw new Error('Not implemented'); // remove this line and put your code here
-};
-
-/**
- * In Array of Users every User under odd index in Array should celebrate his birthday
- * @param {Array<User>} users
- * @return {Array<User>}
- */
-module.exports.celebrate = function (users) {
-  throw new Error('Not implemented'); // remove this line and put your code here
+const celebrate = (users) => {
+  users.forEach((user, index) => {
+      if (index % 2 !== 0) user.celebrateBirthday();
+  });
 };
